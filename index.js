28b14ac5a6bd88fb82626af905e74e667be63492
samuelharold568 <samuelharold327@gmail.com>
2022-05-28 23:57:02 +0700

    memperbaiki bug pdafirstUser

--- index.js
+++ index.js
@@ -2,8 +2,8 @@
 // Tentukan tipe data dan struktur data yang menurut kalian tepat
 // Kalian bisa membuat variabel lain untuk dimasukkan ke dalam atribut user agar lebih mudah dibaca
 const firstUser = {
-    name: 'wendy',
-    gender: 'male',
+    name: 'Monica',
+    gender: 'Female',
     age: 17,
     email: "monica@dingdong.com",
     favoriteColor: ["Yellow", "Pink", "White", "Purple"],
