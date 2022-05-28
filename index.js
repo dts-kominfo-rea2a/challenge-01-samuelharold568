995521d580780d38397c42bcd0b4890a0ed749c6
samuelharold568 <samuelharold327@gmail.com>
2022-05-28 11:58:13 +0700

    menambahkan code didalam file index.js

--- index.js
+++ index.js
@@ -1,7 +1,18 @@
 // TODO: Buatlah data dari kedua user di sini
 // Tentukan tipe data dan struktur data yang menurut kalian tepat
 // Kalian bisa membuat variabel lain untuk dimasukkan ke dalam atribut user agar lebih mudah dibaca
-const firstUser = {};
+const firstUser = {
+    name: 'wendy',
+    gender: 'male',
+    age: 17,
+    email: "monica@dingdong.com",
+    favoriteColor: ["Yellow", "Pink", "White", "Purple"],
+    isHavePet: "Yes",
+    education: {
+        name: "SD 01", city: "Jakarta", graduate: 2016
+    }
+
+};
 const secondUser = {};
 
 // TODO: Masukkan hasil akhir dari kedua user di sini
